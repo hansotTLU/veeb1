9cc21aa29397ac43c5469374084a34433ee821a5
Hans Oskar Trolla <hansotTLU@users.noreply.github.com>
2023-10-12 17:26:57 +0300

    Tund 6 tehtud

---
+++ datetime_et.js
@@ -0,0 +1,34 @@
+/*const monthsET = function (){
+	let monthNamesET = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
+	
+	return monthNamesET
+}*/
+
+const dateETformatted = function(){
+	let timeNow = new Date();
+	const monthNamesET = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
+	return timeNow.getDate() + ". " + monthNamesET[timeNow.getMonth()] + " " + timeNow.getFullYear();
+}
+
+const timeETformatted = function(){
+	let timeNow = new Date();
+	return timeNow.getHours() + ":" + timeNow.getMinutes() + ":" + timeNow.getSeconds();
+}
+
+const timeOfDayET = function(){
+	let partOfDay = "suvaline hetk";
+	let hourNow = new Date().getHours();
+	if (hourNow >= 6 && hourNow < 12){
+		partOfDay = "hommik";
+	}
+	if (hourNow > 14 && hourNow < 18){
+		partOfDay = "pärastlõuna";
+	}
+	if(hourNow >= 18){
+		partOfDay = "õhtu";
+	}
+	return partOfDay;
+}
+
+//ekspordin kõik asjad
+module.exports = {dateETformatted: dateETformatted, timeETformatted: timeETformatted, timeOfDayET: timeOfDayET};
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const timeInfo = require('./datetime_et');
+const fs = require("fs");
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req,res)=>{
 	//res.send('See töötab');
@@ -9,9 +12,24 @@ app.get('/', (req,res)=>{
 	res.render('index');
 });
 
-app.get('/test', (req,res)=>{
-	res.send('Test töötab ka');
-	//res.download('index.js'); #(ligi avamisel saab kindla faili alla laadida)#
+app.get('/timenow', (req,res)=>{
+	const dateNow = timeInfo.dateETformatted();
+	const timeNow = timeInfo.timeETformatted();
+	//res.render('timenow');
+	res.render('timenow', {nowD: dateNow, nowT: timeNow});
+});
+
+app.get('/wisdom', (req,res)=>{
+	let folkWisdom = [];
+	fs.readFile('public/txt_files/vanasonad.txt', 'utf8', (err, data)=>{
+		if(err){
+			throw err;
+		}
+		else {
+			folkWisdom = data.split(';');
+			res.render('justlist', {h1: 'Vanasõnad', wisdom: folkWisdom});
+		}
+	});
 });
 
 app.listen(5121);
